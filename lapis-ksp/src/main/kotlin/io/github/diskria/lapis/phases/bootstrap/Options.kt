cadf54d33bcebfb9f34802d2283d25a116868d1b
diskria <diskria@proton.me>
2026-09-09 13:26:28 +0400

    refactor(ksp): set parent of mixin package as builtins package by default

--- lapis-ksp/src/main/kotlin/io/github/diskria/lapis/phases/bootstrap/Options.kt
+++ lapis-ksp/src/main/kotlin/io/github/diskria/lapis/phases/bootstrap/Options.kt
@@ -5,8 +5,8 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Options(
     val modUniquePrefix: String,
-    val builtinsPackage: String,
     val mixinPackage: String,
+    val builtinsPackage: String = mixinPackage.substringBeforeLast("."),
     val mixinGeneratedSubpackage: String? = null,
     val enableFabricTweaks: Boolean = false,
     val enableForgeTweaks: Boolean = false,
