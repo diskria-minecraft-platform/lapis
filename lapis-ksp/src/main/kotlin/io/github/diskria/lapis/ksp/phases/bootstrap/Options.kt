ffa4445ec4cac5de72717714a7c99dc4c0d1d411
diskria <diskria@proton.me>
2026-09-10 23:05:49 +0400

    feat(ksp): add Java identifier name validation for 'uniqueModPrefix' argument

--- lapis-ksp/src/main/kotlin/io/github/diskria/lapis/ksp/phases/bootstrap/Options.kt
+++ lapis-ksp/src/main/kotlin/io/github/diskria/lapis/ksp/phases/bootstrap/Options.kt
@@ -13,6 +13,9 @@ data class Options(
     val disableLCP: Boolean = false,
 ) {
     init {
+        require(IDENTIFIER_REGEX.matches(uniqueModPrefix)) {
+            "Invalid 'uniqueModPrefix': '$uniqueModPrefix'. Must be a valid Java identifier name."
+        }
         require(PACKAGE_REGEX.matches(mixinPackage)) {
             "Invalid 'mixinPackage': '$mixinPackage'. Must be a valid Java package name."
         }
@@ -22,12 +25,13 @@ data class Options(
         mixinGeneratedSubpackage?.let { subpackage ->
             require(PACKAGE_REGEX.matches(subpackage)) {
                 "Invalid 'mixinGeneratedSubpackage': '$subpackage'. Must be a valid Java package name " +
-                    "without leading or trailing dots (e.g. 'generated' or 'generated.lapis')."
+                    "without leading or trailing dots (e.g. 'generated')."
             }
         }
     }
 
     companion object {
+        private val IDENTIFIER_REGEX = Regex("""^[a-zA-Z_$][a-zA-Z0-9_$]*$""")
         private val PACKAGE_REGEX = Regex("""^[a-zA-Z_][a-zA-Z0-9_]*(\.[a-zA-Z_][a-zA-Z0-9_]*)*$""")
     }
 }
